106a0687493a52904c6ea14593e19114e5d8538f
HM11019 <hm11019@ues.edu.sv>
2022-04-05 02:28:23 -0600

    secund commit

--- app/src/main/java/com/example/hm11019/PruebaViewModel.java
+++ app/src/main/java/com/example/hm11019/PruebaViewModel.java
@@ -3,5 +3,5 @@ package com.example.hm11019;
 import androidx.lifecycle.ViewModel;
 
 public class PruebaViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    // TODO: Implement the ViewModelxcvbxcvbxfsfdg
 }
